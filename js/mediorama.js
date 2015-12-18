03043d6496c9e0b4fb3b4e5d147f8fce4cadf9fc
sempeck <jarek.sepek@mediorama.pl>
2015-12-18 09:13:35 +0100

    Menu zasysane z osobnego pliku

--- js/mediorama.js
+++ js/mediorama.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 		  
 		   "use strict";
 		
+		$("#menu").load("menu.html");	
+		
            createFullpage();
 	      
 	   function createFullpage() {
         $('#fullpage').fullpage({
            anchors: ['intro', 'opis', 'projekty', 'kontakt'],
-				menu: '#menu',
+			//	menu: '#menu',
 				scrollingSpeed: 1000,
 			    slidesNavigation: true,
 			//	scrollOverflow: true
@@ -15,28 +17,28 @@ $(document).ready(function() {
 }
 
 		 
-    var slides;
-   	var v = false;  
-	  
-        $('img').click(function(){
-		  slides = $("#fullpage").detach();
-	      $("#projekt").load("portfolio2.html");	
-    	  $.fn.fullpage.destroy('all');  
-	      //   $("body").css("overflow", "scroll");
-	          v = true; 
-		});
-    
-	
-	
-	    $('.nav').click(function(){
-			if (v) { 
-              $("#projekt").empty(); 
-			   // $.fn.fullpage.reBuild();
-	         $("body").prepend(slides);		 
-			// $("body").css("overflow", "hidden");
-			  v = false;
-			  createFullpage();
-			}});
+ //   var slides;
+//   	var v = false;  
+//	  
+//        $('img').click(function(){
+//		  slides = $("#fullpage").detach();
+//	      $("#projekt").load("portfolio2.html");	
+//    	  $.fn.fullpage.destroy('all');  
+//	      //   $("body").css("overflow", "scroll");
+//	          v = true; 
+//		});
+//    
+//	
+//	
+//	    $('.nav').click(function(){
+//			if (v) { 
+//              $("#projekt").empty(); 
+//			   // $.fn.fullpage.reBuild();
+//	         $("body").prepend(slides);		 
+//			// $("body").css("overflow", "hidden");
+//			  v = false;
+//			  createFullpage();
+//			}});
 	
 		  
 }); 
