1a519dee926dd203dc915f3908a0fc8336c4cf09
sempeck <jarek.sepek@mediorama.pl>
2016-01-07 13:38:54 +0100

    Układ treści

--- js/mediorama.js
+++ js/mediorama.js
@@ -1,50 +1,33 @@
 $(document).ready(function() {
-		
-		   "use strict";
-	
-	$(document).scroll(function () {
-    if ($("body").height() <= ($(window).height() + $(window).scrollTop() + 600)) {
+
+    "use strict";
+
+    $(document).scroll(function () {
+        if ($("body").height() <= ($(window).height() + $(window).scrollTop() + 600)) {
         $('.znik').show();
-    } else {
+        } else {
         $('.znik').hide();
-    }
-});
-	
-	$("#menu").load("menu.html");	
-		
-	
-	$("#powrot").click(function(){
-	window.history.back();
-	});
-	
-		
-           createFullpage();
-	      
-	   function createFullpage() {
+        }
+    });
+
+    $("#menu").load("menu.html");	
+
+    createFullpage();
+
+    function createFullpage() {
         $('#fullpage').fullpage({
-           anchors: ['opis', 'projekty', 'kontakt'],
-			//	menu: '#menu',
-				scrollingSpeed: 1000,
-			    slidesNavigation: true,
-			    //scrollOverflow: true
+        anchors: ['opis', 'projekty', 'kontakt'],
+        //	menu: '#menu',
+        scrollingSpeed: 1000,
+        slidesNavigation: true,
+        scrollOverflow: true,
+        animateAnchor: false
         });
-}
+    }
 
 //animacja przejścia
-     window.addEventListener("beforeunload", function () {
-     document.body.classList.add("animate-out");
-});
+    window.addEventListener("beforeunload", function () {
+    document.body.classList.add("animate-out");
+    });
 
-
- 
-
-	
-// guzik powrotu	
-//	$(window).scroll(function () { 
-//   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
-//	 $(".powrot").css("display", "block");
-//   }
-//});
-
-	  
 }); 
