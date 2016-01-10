9459b419c5e014e3ddf78f85f6c6f2d8b5c71925
sempeck <jarek.sepek@mediorama.pl>
2016-01-10 20:20:34 +0100

    Przeniesiona funkcja

--- js/mediorama.js
+++ js/mediorama.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
 
     "use strict";
 
-
     $(document).scroll(function () {
         if ($("body").height() <= ($(window).height() + $(window).scrollTop() + 600)) {
         $('.znik').css("z-index",1);
@@ -18,19 +17,6 @@ $(document).ready(function() {
     });
 
     $("#menu").load("menu.html");	
-
-    createFullpage();
-
-    function createFullpage() {
-        $('#fullpage').fullpage({
-        anchors: ['opis', 'projekty', 'kontakt'],
-        //	menu: '#menu',
-        scrollingSpeed: 1000,
-        slidesNavigation: true,
-        scrollOverflow: true,
-        animateAnchor: false
-        });
-    }
 
 //animacja przejścia
     window.addEventListener("beforeunload", function () {
